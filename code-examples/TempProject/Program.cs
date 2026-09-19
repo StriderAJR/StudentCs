b59bc7270ded3a1977808cbaa0b3ed44083fe439
StriderAJR <alexander.aj.ranger@gmail.com>
2026-09-19 17:09:39 +0500

    v2 use-git.md

--- code-examples/TempProject/Program.cs
+++ code-examples/TempProject/Program.cs
@@ -10,40 +10,68 @@ namespace TempProject;
 
 internal class Program
 {
-    public static int ConvertToNumber(string buffer)
+    public static int ParseToNumber(string buffer)
     {
-        // ...
-        // нельзя использовать Parse, TryParse, Convert.ToInt32() и т.д. подобное
-        // вернуть число
-
-        // привидение - (int)
-        // преобразование - Parse, Convert.ToInt32()
-
         // "1234"
 
         int result = 0;
-        foreach(char letter in buffer)
+        foreach (char c in buffer)
         {
-            int digit = letter - '0'; // 0 = 48, 1 = 49 ... 
+            int digit = c - '0';
             result = result * 10 + digit;
         }
 
-        // 1 = 1 * 10^3
-        // 2 = 2 * 10^2
-        // 3 = 3 * 10^1
-        // 4
-        // 1234
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char c = buffer[i];
 
-        // 1 => result = 1
-        // 2 => result = 1 * 10 + 2 = 12
-        // 3 => result = 12 * 10 + 3 = 123
-        // ...
+            int digit = c - '0';
+            result = result * 10 + digit;
+        }
 
         return result;
+
+        // 1 = 1
+        // 2 = 1 * 10 + 2 = 12
+        // 3 = 12 * 10 + 3 = 120 + 3 = 123
+        // 4 = 123 * 10 + 4 = 1234
     }
+
 
     public static void Main()
     {
-        int num = ConvertToNumber(Console.ReadLine());
+        int a = 10;
+
+        // for 
+        // while 
+        // do-while
+
+        for (int i = 5; i <= 5; i++)
+        {
+            if (i % 2 == 0) continue;
+
+            Console.WriteLine(i);
+        }
+
+        int i2 = 5;
+        while(a <= 5)
+        {
+            if (i2 % 2 == 0)
+            {
+                i2++;
+                continue;
+            }
+            Console.WriteLine(i2);
+            i2++;
+        }
+
+        do
+        {
+            Console.WriteLine("...");
+        }
+        while (a <= 5);
+
+        int num = Add(1, 2);
+        PrintMessage("Hello");
     }
 }
